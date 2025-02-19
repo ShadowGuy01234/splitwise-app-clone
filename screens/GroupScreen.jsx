3a28f210c9dc10885f51eff4c79f8ecd2e670cb0
ShadowGuy01234 <121357713+ShadowGuy01234@users.noreply.github.com>
2025-02-20 02:05:13 +0530

    completed GroupScreen

--- screens/GroupScreen.jsx
+++ screens/GroupScreen.jsx
@@ -1,6 +1,8 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import GroupCard from "../components/GroupScreenCard";
 import Ionicons from '@expo/vector-icons/Ionicons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 
 export default function GroupScreen({title, amount}) {
@@ -13,7 +15,7 @@ export default function GroupScreen({title, amount}) {
             <Text style={{
                 flex: 1,
                 fontSize: 18,
-            }}>Overall, You own nothing!</Text>
+            }}>Overall, You own nothing! 😼</Text>
             <Ionicons name="filter-sharp" size={24} color="black" />
 
         </View>
@@ -21,7 +23,44 @@ export default function GroupScreen({title, amount}) {
         <GroupCard title='2' expense = '20' />
         <GroupCard title='3' expense = '110' />
         <GroupCard title='4' expense = '200' />
-
+        
+        <View style={{
+            // width: 50,
+        }}>
+            <Pressable style={{
+            flexDirection: "row",
+            gap: 5,
+            justifyContent:"center",
+            margin: 5,
+            borderWidth: 2,
+            borderRadius: 50,
+            alignItems: 'center',
+            paddingVertical: 5,
+        }}>
+            <AntDesign name="adduser" size={24} color="black" />
+                <Text>Start a new Group</Text>
+            </Pressable>
+        </View>
+        <View>
+            <Pressable style={{
+            flexDirection: "row",
+            gap: 8,
+            alignItems: "center",
+            backgroundColor: "green",
+            width: 150,
+            justifyContent:"center",
+            borderRadius: 50,
+            paddingVertical: 5,
+            left: 200,
+            top: 110
+        }}>
+            <FontAwesome6 name="add" size={24} color="white" />
+            <Text style={{
+                fontWeight: 500,
+                color: "white"
+            }}>Add expense</Text>
+            </Pressable>
+        </View>
         </>
     );
 }
